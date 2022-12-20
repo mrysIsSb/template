2d9a06d40c140d67f50963cc51af2c914a3262e9
mrys <1510218499@qq.com>
2022-12-20 14:51:39 +0800

    更新

--- mrys-custom/mrys-security/src/test/java/top/mrys/TestFlux.java
+++ mrys-custom/mrys-security/src/test/java/top/mrys/TestFlux.java
@@ -2,16 +2,14 @@ package top.mrys;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.reactive.server.WebTestClient;
-import org.springframework.web.reactive.config.EnableWebFlux;
 
 /**
  * @author mrys
  * @date 2022/12/16 15:35
  */
-@SpringBootTest(properties = "spring.main.web-application-type=reactive")
-@EnableWebFlux
+//@SpringBootTest(properties = "spring.main.web-application-type=reactive")
+//@EnableWebFlux
 public class TestFlux {
 
     @Test
