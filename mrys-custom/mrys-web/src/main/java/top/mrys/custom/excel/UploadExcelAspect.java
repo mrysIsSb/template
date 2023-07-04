4f9913c6b9611e4162149d69e798e96cf2de56d1
mrys <1510218499@qq.com>
2023-07-04 13:15:40 +0800

    excel

--- mrys-custom/mrys-web/src/main/java/top/mrys/custom/excel/UploadExcelAspect.java
+++ mrys-custom/mrys-web/src/main/java/top/mrys/custom/excel/UploadExcelAspect.java
@@ -56,7 +56,7 @@ public class UploadExcelAspect {
       Part part = request.getPart(StrUtil.blankToDefault(uploadExcel.value(), "file"));
 
       InputStream stream = part.getInputStream();
-      List excel = util.importExcel(stream, 2);
+      List excel = util.importExcel(stream, 1);
       args[0] = excel;
 
       return joinPoint.proceed(args);
@@ -86,7 +86,7 @@ public class UploadExcelAspect {
       }
       return detail;
     });
-    util.exportExcel(requestAttributes.getResponse(), Collections.emptyList(),"sheet1","模板");
+    util.exportExcel(requestAttributes.getResponse(), Collections.emptyList(),"sheet1");
   }
 
 }
